64cab7ff0d6c1f18c3dfc8a9f144b7175e4a7dd4
PawanKmr470 <pawankmr470@gmail.com>
2022-08-23 02:34:23 +0530

    Stack using Linked List

--- py_drill/DS/Stack.py
+++ py_drill/DS/Stack.py
@@ -4,8 +4,10 @@ class Stack:
     """Stack using list"""
     def __init__(self):
         self.head = None
+        # Take self.size = 0 variable to maintain size
 
     def push(self, data):
+        # insert at begining
         new_node = Node(data)
         if self.is_empty():
             self.head = new_node
@@ -17,6 +19,7 @@ class Stack:
         return None
 
     def pop(self):
+        # remove from begining
         if self.is_empty():
             print("Stack is empty, Nothing to pop.")
         else:
@@ -26,10 +29,7 @@ class Stack:
             return tmp_node.data
 
     def is_empty(self):
-        if self.head is None:
-            return True
-        else:
-            return False
+        return True if self.head is None else False
 
     def print_stack(self):
         if self.is_empty():
