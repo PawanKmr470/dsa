2b33a7dd9be55c559ad045e4536e0e7428bfeacc
PawanKmr470 <pawankmr470@gmail.com>
2022-08-22 14:31:37 +0530

    Node class

---
+++ py_drill/DS/Node.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, data, pnext=None):
+        self.data = data
+        self.pnext = pnext
+
+    def __repr__(self):
+        return "Node(data, pnext)"
+
+    def __str__(self):
+        return f"{self.data} {self.pnext}"
