d7de8a14ac172032473752c3f9effd15a937e37f
PawanKmr470 <pawankmr470@gmail.com>
2022-08-23 03:36:19 +0530

    Node class

--- py_drill/DS/Node.py
+++ py_drill/DS/Node.py
@@ -9,10 +9,24 @@ class Node:
     def __str__(self):
         return f"{self.data} {self.pnext}"
 
+class BSTNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def __repr__(self):
+        return "tNode(data, left, right)"
+
+    def __str__(self):
+        return f"data : {self.data}, left : {self.left}, right : {self.right}"
+
 
 def main():
     n1 = Node(10)
     print(n1)
+    t1 = BSTNode(20)
+    print(t1)
 
 if __name__ == "__main__":
     main()
