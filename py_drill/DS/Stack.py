33bb9458d7113b54b65faf656f7a72af8b78536c
PawanKmr470 <pawankmr470@gmail.com>
2022-08-23 01:01:30 +0530

    Stack using Linked List

---
+++ py_drill/DS/Stack.py
@@ -0,0 +1,59 @@
+from Node import Node
+
+class Stack:
+    """Stack using list"""
+    def __init__(self):
+        self.head = None
+
+    def push(self, data):
+        new_node = Node(data)
+        if self.is_empty():
+            self.head = new_node
+            new_node.pnext = None
+        else:
+            tmp_node = self.head
+            self.head = new_node
+            new_node.pnext = tmp_node
+        return None
+
+    def pop(self):
+        if self.is_empty():
+            print("Stack is empty, Nothing to pop.")
+        else:
+            tmp_node = self.head
+            self.head = self.head.pnext
+            tmp_node.pnext = None
+            return tmp_node.data
+
+    def is_empty(self):
+        if self.head is None:
+            return True
+        else:
+            return False
+
+    def print_stack(self):
+        if self.is_empty():
+            print("Stack is empty, Nothing to print.")
+        else:
+            cur_node = self.head
+            while cur_node:
+                print(cur_node.data)
+                cur_node = cur_node.pnext
+        return None
+
+
+def main():
+    s1 = Stack()
+    s1.push(2)
+    s1.push(3)
+    s1.push(4)
+    print("Printing stack :")
+    s1.print_stack()
+    print("Poping last element")
+    s1.pop()
+    print("Printing stack :")
+    s1.print_stack()
+
+
+if __name__ == "__main__":
+    main()
