a0d383d124c86c4f6857caed5625b000c74d4d99
PawanKmr470 <pawankmr470@gmail.com>
2022-08-26 14:57:58 +0530

    BST and traversals

--- py_drill/DS/BST.py
+++ py_drill/DS/BST.py
@@ -1,4 +1,5 @@
 from Node import BSTNode
+from collections import deque
 
 class BST:
     def __init__(self):
@@ -47,6 +48,7 @@ class BST:
     # Preorder Traversal
     #
     def print_preorder(self):
+        result = []
         if self.root is None:
             print("BST is empty.")
             return
@@ -55,13 +57,15 @@ class BST:
         stack.append(self.root)
         while len(stack) > 0:
             node = stack.pop()
-            print("Preorder element : {}".format(node.data))
+            result.append(node.data)
 
             # NOTE : push right first bcz it will be popped at last or visited last
             if node.right:
                 stack.append(node.right)
             if node.left:
                 stack.append(node.left)
+
+        print("Preorder element : {}".format(result))
 
     def print_preorder_rec(self):
         if self.root is None:
@@ -128,13 +132,77 @@ class BST:
         self.__print_postorder_rec(node.right)
         print("Postorder rec element : {}".format(node.data))
 
+    #
+    # Levelorder Traversal
+    #
+    def print_levelorder(self):
+        if self.root is None:
+            print("BST is empty.")
+            return
+
+        q = deque()
+        cur_node = self.root
+        q.append(cur_node)
+        while len(q):
+            cur_node = q.popleft()
+            print("Levelorder element : {}".format(cur_node.data))
+            if cur_node.left:
+                q.append(cur_node.left)
+            if cur_node.right:
+                q.append(cur_node.right)
+
+    # It doesn't use queue
+    # Implemented with calculating height and then traversing all nodes at each level
+    def print_levelorder_rec(self):
+        return
+
+    # Height using recursion
+    def print_height_rec(self):
+        bst_height = self.__print_height_rec(self.root)
+        print("Height rec of BST is : {}".format(bst_height))
+
+    def __print_height_rec(self, node):
+        if node is None:
+            return -1
+        return 1 + max(self.__print_height_rec(node.left), self.__print_height_rec(node.right))
+
+    # Height using queue (iterative)
+    def print_height(self):
+        bst_height = self.__print_height()
+        print("Height of BST is : {}".format(bst_height))
+
+    def __print_height(self):
+        if self.root is None:
+            return -1
+
+        height = 0
+        q = deque()
+        q.append(self.root)
+
+        while True:
+            node_count = len(q)
+            if node_count == 0:
+                return height - 1
+
+            height += 1
+
+            while node_count:
+                cur_node = q.popleft()
+                if cur_node.left:
+                    q.append(cur_node.left)
+                if cur_node.right:
+                    q.append(cur_node.right)
+                node_count -= 1
+
 
 def main():
     bst = BST()
     bst_rec = BST()
 
-    bst.insert([5, 1, 10])
-    bst_rec.insert_rec([50, 10, 100])
+    bst.insert([5, 1, 10, 100, 200, 300, 400])
+    bst_rec.insert([5, 1, 10, 100, 200, 300, 400])
+    # bst.insert_([50, 10, 100])
+    # bst_rec.insert_rec([50, 10, 100])
 
     print("****************************")
     bst.print_preorder()
@@ -143,7 +211,16 @@ def main():
     bst.print_inorder()
     bst_rec.print_inorder_rec()
     print("****************************")
+    bst.print_postorder()
     bst_rec.print_postorder_rec()
+    print("****************************")
+    bst.print_levelorder()
+    print("****************************")
+    bst.print_height()
+    bst_rec.print_height_rec()
+    # bst1 = BST()
+    # bst1.insert([5])
+    # bst1.print_height()
 
 
 if __name__ == "__main__":
