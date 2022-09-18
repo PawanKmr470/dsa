5d43fc09e96bfd3a15ede97c92c0609c20820f46
PawanKmr470 <pawankmr470@gmail.com>
2022-09-18 06:26:39 +0530

    Disjoint set + Union by rank + path compression

---
+++ py_drill/Problems/DSet_NoOfComponents.py
@@ -0,0 +1,52 @@
+# REF : https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+# NOTES :
+#   Rank doesn't let form skew tree
+#   Path compression reduces the tree height and converges O(log n) to O(1)
+#   For cycle detection keep unioning the edges and check for p1 == p2
+
+# T: O(log n)
+# S: O(
+
+class Solution:
+    def countComponents(self, n: int, edges: list[list[int]]) -> int:
+        parent = [i for i in range(n)]
+        rank = [1] * n
+
+        def find(n):
+            res = n
+            while res != parent[res]:
+                parent[res] = parent[parent[res]]   # Path compresssion
+                res = parent[res]
+            return res
+
+        def union(n1, n2):
+
+            p1 = find(n1)
+            p2 = find(n2)
+
+            if p1 == p2:
+                return 0
+
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1         # Make p1 parent of p2
+                rank[p1] += rank[p2]    # Increase p1's rank with p2
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+
+            return 1
+
+        res = n
+        for n1, n2 in edges:
+            res -= union(n1, n2)
+        return res
+
+
+def main():
+    n = 5
+    edges = [[0,1],[1,2],[3,4]]
+
+    print("No of components : {}".format(Solution().countComponents(n, edges)))
+
+if __name__ == '__main__':
+    main()
