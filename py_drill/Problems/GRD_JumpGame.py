690ab1096826fbbf5cf343572daa60b538e6358d
PawanKmr470 <pawankmr470@gmail.com>
2022-09-20 00:39:39 +0530

    Greedy approach

---
+++ py_drill/Problems/GRD_JumpGame.py
@@ -0,0 +1,25 @@
+# REF : https://leetcode.com/problems/jump-game/
+# NOTES :
+#       Here Greedy is preferred over DP.
+#       If the question asks min no of hops to reach end index instead of returing T/F,
+#       may be DP could solve that but with same idea starting from end to index 0.
+
+# T: O(n)
+# S: O(1)
+
+class Solution:
+    def canJump(self, nums: list[int]) -> bool:
+        goal = len(nums) - 1
+        for i in range(len(nums) - 1, -1, -1):
+            if i + nums[i] >= goal:
+                goal = i
+
+        return True if goal == 0 else False
+
+
+def main():
+    nums = [2,3,1,1,4]
+    print("max sumarray sum : {}".format(Solution().canJump(nums)))
+
+if __name__ == "__main__":
+    main()
