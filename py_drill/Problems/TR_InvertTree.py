9d996820973d849a41d7aef1a407c305799f0a68
PawanKmr470 <pawankmr470@gmail.com>
2022-09-23 17:41:30 +0530

    Tree + Any({Pre,In,Post) traversal recursion

---
+++ py_drill/Problems/TR_InvertTree.py
@@ -0,0 +1,24 @@
+# REF : https://leetcode.com/problems/invert-binary-tree/
+# NOTES :
+#       Just preorder traversal and swap the elements
+#       Actually any order traversal works :D
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def invertTree(self, root: TreeNode):
+        if root is None:
+            return None
+
+        root.left, root.right = root.right, root.left
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+
+        return root
+
