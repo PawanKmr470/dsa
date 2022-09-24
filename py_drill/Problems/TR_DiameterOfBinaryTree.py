81589d15f559cbc1ff1488e613f9aa151c9881af
PawanKmr470 <pawankmr470@gmail.com>
2022-09-24 15:59:13 +0530

    DFS + longest path

---
+++ py_drill/Problems/TR_DiameterOfBinaryTree.py
@@ -0,0 +1,61 @@
+# REF : https://leetcode.com/problems/diameter-of-binary-tree/
+# NOTES :
+#       while stack unwinding, calculate height of the node and just before that
+#       keep storing the max result
+
+# T: O(n)
+# S: O(n)
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+# Working but not efficient
+# T: O(n^2) due to height calculation
+# S: O(n)
+class Solution1:
+    def diameterOfBinaryTree(self, root):
+        if root is None:
+            return 0
+
+        left_dia = self.diameterOfBinaryTree(root.left)
+        right_dia = self.diameterOfBinaryTree(root.right)
+
+        cur = self.height(root.left) + self.height(root.right)
+
+        return max(cur, max(left_dia, right_dia))
+
+    def height(self, root):
+        if root is None:
+            return 0
+        return 1 + max(self.height(root.left), self.height(root.right))
+
+
+# T: O(n)
+# S: O(n)
+class Solution2:
+    def diameterOfBinaryTree(self, root):
+        result = []
+        result.append(0)
+
+        def heightwithself(root):
+            if root is None:
+                return 0
+
+            lh = heightwithself(root.left)
+            rh = heightwithself(root.right)
+
+            result[0] = max(result[0], lh + rh)
+
+            return 1 + max(lh, rh)
+
+        heightwithself(root)
+        return result[0]
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
