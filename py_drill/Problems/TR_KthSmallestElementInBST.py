76dcba4bdff14d6ed2ce5915b1eb466905c8283c
PawanKmr470 <pawankmr470@gmail.com>
2022-09-25 04:54:06 +0530

    BST Inorder Traversal

---
+++ py_drill/Problems/TR_KthSmallestElementInBST.py
@@ -0,0 +1,33 @@
+# REF : https://leetcode.com/problems/kth-smallest-element-in-a-bst/solution/
+# NOTES :
+#       Inorder traversal of BST gives sorted output
+
+# T: O(n)
+# S: O(n)
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def kthSmallest(self, root, k) -> int:
+        sortlist = []
+
+        def inorder(node):
+            if node is None:
+                return
+
+            inorder(node.left)
+            sortlist.append(node.val)
+            inorder(node.right)
+
+        inorder(root)
+        return sortlist[k - 1]
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
