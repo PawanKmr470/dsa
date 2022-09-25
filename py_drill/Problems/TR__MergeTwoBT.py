2bb74f191e09d0cbb6d92eabb5deab553b0763bb
PawanKmr470 <pawankmr470@gmail.com>
2022-09-26 02:58:07 +0530

    DFS + preorder + new tree + merge into first

--- py_drill/Problems/TR__MergeTwoBT.py
+++ py_drill/Problems/TR__MergeTwoBT.py
@@ -17,7 +17,7 @@ class Solution:
             return None
 
         v1 = root1.val if root1 else 0
-        v2 = root1.val if root2 else 0
+        v2 = root2.val if root2 else 0
         root = TreeNode(v1 + v2)
 
         if root1 and root2:
@@ -38,6 +38,23 @@ class Solution:
         # root.left = self.mergeTrees(root1.left if root1 else None, root2.left if root2 else None)
         # root.right = self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None)
         # return root
+
+
+# Merging root2 into root1. Not separate BT
+class Solution2:
+    def mergeTrees(self, root1, root2):
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+
+        root1.val += root2.val
+
+        root1.left = self.mergeTrees(root1.left if root1 else None, root2.left if root2 else None)
+        root1.right = self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None)
+
+        return root1
+
 
 def main():
     pass
