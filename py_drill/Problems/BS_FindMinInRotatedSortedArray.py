a477c48e0e1b7893f3adfd4b9b587b84372eb715
PawanKmr470 <pawankmr470@gmail.com>
2022-10-02 04:15:43 +0530

    Binary search for finding unsorted subarray

--- py_drill/Problems/BS_FindMinInRotatedSortedArray.py
+++ py_drill/Problems/BS_FindMinInRotatedSortedArray.py
@@ -28,9 +28,29 @@ class Solution:
 
         return -1
 
+class Solution2:
+    def findMin(self, nums: list[int]) -> int:
+        res = nums[0]
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            if nums[l] <= nums[r]:
+                res = min(res, nums[l])
+                break
+
+            mid = (l + r) // 2
+            res = min(res, nums[mid])
+            if nums[l] <= nums[mid]:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return res
+
 
 def main():
     print("output : {}".format(Solution().findMin([4,5,6,7,0,1,2])))
+    print("output : {}".format(Solution2().findMin([4,5,6,7,0,1,2])))
 
 if __name__ == "__main__":
     main()
