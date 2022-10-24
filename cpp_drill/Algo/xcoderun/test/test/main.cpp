758144ebfe18387ed4d9464c1a462996a091d3de
PawanKmr470 <pawankmr470@gmail.com>
2022-10-25 01:18:45 +0530

    Pointers

---
+++ cpp_drill/Algo/xcoderun/test/test/main.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+Basic recursion solution.
+T - O(2^n)
+S - O(2^n)
+*/
+class Solution1 {
+public:
+    int lcs(string s1, string s2) {
+        int result = lcs_rec(s1, s2, s1.length(), s2.length());
+        return result;
+    }
+
+    int lcs_rec(string &str1, string &str2, int len1, int len2) {
+        if (len1 == 0 || len2 == 0) {
+            return 0;
+        }
+        if (str1[len1 - 1] == str2[len2 - 1]) {
+            return 1 + lcs_rec(str1, str2, len1 - 1, len2 - 1);
+        }
+        else {
+            return max(lcs_rec(str1, str2, len1, len2 - 1),
+                         lcs_rec(str1, str2, len1 - 1, len2));
+        }
+    }
+};
+
+/*
+Recursion solution starting from index 0.
+Earlier one is traversing backwards.
+T - O(2^n)
+S - O(2^n)
+*/
+class Solution2 {
+public:
+    int lcs(string s1, string s2) {
+        int result = lcs_rec(s1, s2, 0, 0);
+        return result;
+    }
+
+    int lcs_rec(string &str1, string &str2, int len1, int len2) {
+        if (len1 == (str1.length()) || len2 == (str2.length())) {
+            return 0;
+        }
+        if (str1[len1] == str2[len2]) {
+            return 1 + lcs_rec(str1, str2, len1 + 1, len2 + 1);
+        }
+        else {
+            return max(lcs_rec(str1, str2, len1, len2 + 1),
+                         lcs_rec(str1, str2, len1 + 1, len2));
+        }
+    }
+};
+
+/*
+Recursion solution with memoization - DP.
+T - O(n)
+S - O(n)
+*/
+class Solution3 {
+public:
+    int lcs(string s1, string s2) {
+        vector<vector<int>> vec(s1.length() + 1, vector<int> (s2.length() + 1, -1));
+        int result = lcs_rec(s1, s2, s1.length(), s2.length(), vec);
+        return result;
+    }
+
+    int lcs_rec(string &str1, string &str2, int len1, int len2, vector<vector<int>> &vec) {
+        if (len1 == 0 || len2 == 0) {
+            return 0;
+        }
+        if (vec[len1][len2] != -1) {
+            return vec[len1][len2];
+        }
+        if (str1[len1 - 1] == str2[len2 - 1]) {
+            vec[len1][len2] = 1 + lcs_rec(str1, str2, len1 - 1, len2 - 1, vec);
+        }
+        else {
+            vec[len1][len2] = max(lcs_rec(str1, str2, len1, len2 - 1, vec),
+                         lcs_rec(str1, str2, len1 - 1, len2, vec));
+        }
+        return vec[len1][len2];
+    }
+};
+
+/*
+Solution using tabulation - DP And find the LCS string also.
+T - O(mxn)
+S - O(mxn)
+*/
+class Solution4 {
+public:
+    int lcs(string s1, string s2) {
+        if (s1.empty() || s2.empty()) return 0;
+        vector<vector<int>> dp(s1.length() + 1, vector<int> (s2.length() + 1, 0));
+        cout << s1.length() << endl;
+        int row, col;
+        for (row = 1; row <= s1.length(); row++) {
+            for (col = 1; col <= s2.length(); col++) {
+                if (s1[row - 1] == s2[col - 1]) {
+                    dp[row][col] = dp[row - 1][col - 1] + 1;
+                }
+                else {
+                    dp[row][col] = max(dp[row - 1][col], dp[row][col - 1]);
+                }
+            }
+        }
+        return dp[row - 1][col - 1];
+    }
+
+    vector<char> get_lcs(string s1, string s2) {
+        vector<char> result;
+        if (s1.empty() || s2.empty()) return result;
+        vector<vector<int>> dp(s1.length() + 1, vector<int> (s2.length() + 1, 0));
+        int row, col;
+        for (row = 1; row <= s1.length(); row++) {
+            for (col = 1; col <= s2.length(); col++) {
+                if (s1[row - 1] == s2[col - 1]) {
+                    dp[row][col] = dp[row - 1][col - 1] + 1;
+                }
+                else {
+                    dp[row][col] = max(dp[row - 1][col], dp[row][col - 1]);
+                }
+            }
+        }
+
+        result = buildString(dp, s1);
+        return result;
+    }
+
+    vector<char> buildString(vector<vector<int>> &dp, string s1) {
+        vector<char> result;
+        int row = dp.size() - 1;
+        int col = dp[0].size() - 1;
+        while (row != 0 && col != 0) {
+            if (dp[row][col] == dp[row - 1][col]) {
+                row--;
+            }
+            else if (dp[row][col] == dp[row][col - 1]) {
+                col--;
+            }
+           else {
+                result.insert(result.begin(), s1[row - 1]);
+                row--;
+                col--;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+//    Solution1 s1;
+//    int result = s1.lcs("pawan", "pawn");
+//    cout << "result : " << result << endl;
+//
+//    Solution2 s2;
+//    result = s2.lcs("pawan", "pawn");
+//    cout << "result : " << result << endl;
+//
+//    Solution3 s3;
+//    result = s3.lcs("pawan", "pawn");
+//    cout << "result : " << result << endl;
+
+    Solution4 s4;
+    int result = s4.lcs("pawan", "pawon");
+    cout << "result : " << result << endl;
+
+//    vector<char> lcs_string = s4.get_lcs("pawan", "pawn");
+//    cout << "LCS string is : ";
+//    for (char &i: lcs_string) {
+//        cout << i;
+//    }
+//    cout << endl;
+
+    return 0;
+}
---
+++ cpp_drill/OOPs_and_OS/Pointers.cpp
@@ -0,0 +1,333 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int *ptr1 = new int(10);    // initialize allocated mem with value 10
+    cout << ptr1 << " -> " << *ptr1 << endl;
+
+    int *myptr = new int(10);
+    // int *gptr = myptr;
+    // int *ggptr = gptr;   // it doesn't work this way. ggptr is same as gptr
+    int **gptr = &myptr;
+    int ***sgptr = &gptr;
+    int ****ssgptr = &sgptr; // correct way.
+                             // Pointer should be of appropriate base type
+
+    cout << "************* Memo ***************" << endl;
+    char names[][5] = {"paul", "john"};
+    for (int i=0; i<2; i++) {
+        cout << names[i] << " ";
+    }
+    cout << endl;
+
+    char *name_ptr[2];                          // TO REMEMBER : array of pointers
+    for (int i=0; i<2; i++) {
+        name_ptr[i] = names[i];
+    }
+
+    for (int i=0; i<2; i++) {
+        // cout << name_ptr[i] << endl;
+        cout << *(name_ptr + i) << " ";
+    }
+    cout << endl;
+
+
+
+
+    cout << endl;
+    cout << "************ int arrays **********" << endl;
+
+    // 1D array by array name
+    int arr[5] = {10,20,30,40,50};
+    
+    cout << "pointer size : " << sizeof(int *) << endl;   // on this machine, int ptr size => 8 bytes
+    cout << "integer size : " << sizeof(int) << endl;   // int size => 4 bytes
+
+    cout << "size of arr  : " << sizeof(arr) << endl;           // 20 bytes
+    cout << "size of &arr : " << sizeof(&arr) << endl;          // 8 bytes
+    cout << "typeid.name  : " << typeid(arr).name() << endl;    // A5_i
+    cout << "typeid.name  : " << typeid(&arr).name() << endl;   // PA5_i
+
+    cout << "addr of arr      : " << arr << endl;        // addr of it's first element
+    cout << "addr of arr + 1  : " << arr + 1 << endl;    // skips by 4 bytes (int size)
+    cout << "addr of &arr     : " << &arr << endl;       // also addr of first element AND &arr == 'int (*)[5]' (pointer to an array of 5 items)
+    cout << "addr of &arr + 1 : " << &arr + 1 << endl;   // skips by size of array 20 bytes
+
+    // to check the same
+    cout << "bytes diff1 : " << (char *)(arr + 1) - (char *)arr << endl;    // 4 bytes
+    cout << "bytes diff2 : " << (char *)(&arr + 1) - (char *)&arr << endl;  // 20 bytes
+
+    for (int i=0; i<5; i++) {
+        // cout << arr[i] << " ";       // baisc method
+        cout << *(arr + i) << " ";      // using arr name, arr + 1 is incremented by the size of it's base type
+    }
+    cout << endl;
+
+
+    cout << endl;
+    cout << "********** array with pointer *****" << endl;
+    // 1D array with pointer
+    int *ptr = arr;         // arr is the addr of first element, so int *ptr can take it.
+    
+    // int *ptr2 = &arr;    // ERROR : cannot convert 'int (*)[5]' to 'int*'
+    // int **ptr2 = &arr;   // ERROR : cannot convert 'int (*)[5]' to 'int**'
+    int (*ptr2)[5] = &arr;  // Correct way - pointer to int array
+                            // &arr is the addr of WHOLE array (tough it's same addr as arr),
+                            // so int (*ptr2)[5] will take it not simple int *ptr2
+
+    cout << "addr of ptr      : " << ptr << endl;
+    cout << "addr of ptr + 1  : " << ptr + 1 << endl;   // skips by 4 bytes
+    cout << "addr of ptr2     : " << ptr2 << endl;
+    cout << "addr of ptr2 + 1 : " << ptr2 + 1 << endl;  // skips by 20 bytes (by whole array size)
+
+    cout << "bytes diff3 : " << (char *)(ptr + 1) - (char *)ptr << endl;    // 4 bytes
+    cout << "bytes diff4 : " << (char *)(ptr2 + 1) - (char *)&arr << endl;  // 20 bytes
+
+    cout << "size of ptr   : " << sizeof(ptr) << endl;      // 8 bytes, ptr a pointer only
+    cout << "size of ptr2  : " << sizeof(ptr2) << endl;     // 8 bytes, ptr2 is only pointer
+    cout << "size of *ptr2 : " << sizeof(*ptr2) << endl;    // *ptr2 -> arr
+
+    for (int i=0; i<5; i++) {
+        cout << *(ptr + i) << " ";
+    }
+    cout << endl;
+
+    for (int i=0; i<5; i++) {
+        cout << *(*ptr2 + i) << " ";
+    }
+    cout << endl;
+
+
+
+    cout << endl;
+    cout << "************ 2D arrays ***********" << endl;
+
+    // 2D array
+    int arr2[2][3] = {
+        {10,20,30},
+        {100,200,300}
+    };
+    
+    cout << "size of arr2  : " << sizeof(arr2) << endl;     // 24 bytes (whole array size)
+    cout << "size of &arr2 : " << sizeof(&arr2) << endl;    // 8 bytes (size of pointer)
+
+    cout << "addr of arr2      : " << arr2 << endl;        // addr of FIRST ROW (in fact is also the addr of first element)
+    cout << "addr of arr2 + 1  : " << arr2 + 1 << endl;    // skips by 12 bytes (row size) to second row
+    cout << "addr of &arr2     : " << &arr2 << endl;       // addr of WHOLE 2D array (also addr of first element) AND &arr => 'int (*)[5]'
+    cout << "addr of &arr2 + 1 : " << &arr2 + 1 << endl;   // skips by size of 2d array 24 bytes
+
+    cout << "bytes diff5 : " << (char *)(arr2 + 1) - (char *)arr2 << endl;      // 12 bytes
+    cout << "bytes diff6 : " << (char *)(&arr2 + 1) - (char *)&arr2 << endl;    // 24 bytes
+
+    for (int i=0; i<2; i++) {
+        for (int j=0; j<3; j++) {
+            // cout << arr2[i][j] << " ";
+            cout << *(*(arr2 + i) + j) << " ";
+            // arr2 + 1 will jump to addr of second WHOLE row               => $$ IMPORTANT $$
+            // *(arr2 + 1) will give first element's addr of first row
+            // *(arr2 + 1) + 1 will give second element's addr of first row
+            // *(*(arr2 + 1) + 1) will give actual second element of second row
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "******** 2D array pointer ********" << endl;
+
+    // int **ptr3 = arr2;  // ERROR : cannot convert 'int (*)[3]' to 'int**'
+    // int (*ptr3)[2] = arr2;  // ERROR : cannot convert 'int (*)[3]' to 'int (*)[2]'
+    // int (*ptr3)[cols]
+    int (*ptr3)[3] = arr2;
+    int (*ptr4)[2][3] = &arr2;  // pointer to a matrix of 2x3
+
+    cout << "addr of ptr3      : " << ptr3 << endl;
+    cout << "addr of ptr3 + 1  : " << ptr3 + 1 << endl;     // by row size (12 bytes)
+    cout << "addr of ptr4      : " << ptr4 << endl;
+    cout << "addr of ptr4 + 1  : " << ptr4 + 1 << endl;     // by whole array size (24 bytes)
+
+    cout << "size of ptr3  : " << sizeof(ptr3) << endl;     // pointer size (8 bytes)
+    cout << "size of *ptr3 : " << sizeof(*ptr3) << endl;    // row size (12 bytes)
+    cout << "size of ptr4  : " << sizeof(ptr4) << endl;     // pointer size (8 bytes)
+    cout << "size of *ptr4 : " << sizeof(*ptr4) << endl;    // whole array size (24 bytes)
+
+    cout << "bytes diff7 : " << (char *)(ptr3 + 1) - (char *)ptr3 << endl;  // row size (12 bytes)
+    cout << "bytes diff8 : " << (char *)(ptr4 + 1) - (char *)ptr4 << endl;  // whole array size (24 bytes)
+
+    // using ptr3
+    for (int i=0; i<2; i++) {
+        for (int j=0; j<3; j++) {
+            cout << *( *(ptr3 + i) + j ) << " ";
+            // theory is same as remark : $$ IMPORTANT $$
+        }
+        cout << endl;
+    }
+
+    // using ptr4
+    for (int i=0; i<2; i++) {
+        for (int j=0; j<3; j++) {
+            cout << *( *(*ptr4 + i) + j ) << " ";
+        }
+        cout << endl;
+    }
+    
+    
+
+
+    cout << endl;
+    cout << "************ 3D arrays ***********" << endl;
+
+    // 3D array
+    int arr3[2][3][4] = {
+        {
+            {10,11,12,13},
+            {20,21,22,23},
+            {30,31,32,33},
+        },
+        {
+            {100,110,120,130},
+            {200,210,220,230},
+            {300,310,320,330},
+        }
+    };
+
+    cout << "size of arr3    : " << sizeof(arr3) << endl;       // 96 bytes (whole array size)
+    cout << "size of *arr3   : " << sizeof(*arr3) << endl;      // 48 bytes (first matrix size)
+    cout << "size of **arr3  : " << sizeof(**arr3) << endl;     // 16 bytes (size of first row of first matrix)
+    cout << "size of ***arr3 : " << sizeof(***arr3) << endl;    // 4 bytes (size of first element)
+    cout << "size of &arr3   : " << sizeof(&arr3) << endl;      // 8 bytes (size of pointer)
+
+    cout << "addr of arr3      : " << arr3 << endl;        // addr of FIRST 3x4 matrix (in fact is also the addr of first element)
+    cout << "addr of arr3 + 1  : " << arr3 + 1 << endl;    // skips by 48 bytes (first matrix size) to second matrix
+    cout << "addr of &arr3     : " << &arr3 << endl;       // addr of WHOLE 3D array (also addr of first element)
+    cout << "addr of &arr3 + 1 : " << &arr3 + 1 << endl;   // skips by size of 3D array 96 bytes
+
+    for (int i=0; i<2; i++) {
+        for (int j=0; j<3; j++) {
+            for (int k=0; k<4; k++) {
+                // cout << arr3[i][j][k] << " ";    // basic method
+                cout << *( *( *(arr3 + i) + j ) + k) << " ";
+            }
+            cout << endl;
+            // cout << arr2[i][j] << " ";
+            // cout << *(*(arr2 + i) + j) << " ";
+            // arr2 + 1 will jump to addr of second WHOLE row               => $$ IMPORTANT $$
+            // *(arr2 + 1) will give first element's addr of first row
+            // *(arr2 + 1) + 1 will give second element's addr of first row
+            // *(*(arr2 + 1) + 1) will give actual second element of second row
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+    cout << "******** 3D array pointer ********" << endl;
+
+    // int arr3[2][3][4] = {
+    //     {
+    //         {10,11,12,13},
+    //         {20,21,22,23},
+    //         {30,31,32,33},
+    //     },
+    //     {
+    //         {100,110,120,130},
+    //         {200,210,220,230},
+    //         {300,310,320,330},
+    //     }
+    // };
+
+    // int (*ptrx)[4] = arr3;  // cannot convert 'int (*)[3][4]' to 'int (*)[4]'
+    // int (*ptrx)[][4] = arr3;    // also works
+    int (*ptrx)[3][4] = arr3;
+
+    // int (*ptry)[][3][4] = &arr3;    // also works
+    int (*ptry)[2][3][4] = &arr3;
+
+    cout << "addr of ptrx      : " << ptrx << endl;
+    cout << "addr of ptrx + 1  : " << ptrx + 1 << endl;     // by first matrix size (48 bytes)
+    cout << "addr of ptry      : " << ptry << endl;
+    cout << "addr of ptry + 1  : " << ptry + 1 << endl;     // by whole array size (96 bytes)
+
+    cout << "size of ptrx    : " << sizeof(ptrx) << endl;       // pointer size (8 bytes)
+    cout << "size of *ptrx   : " << sizeof(*ptrx) << endl;      // 48 bytes (size of first matrix)
+    cout << "size of **ptrx  : " << sizeof(**ptrx) << endl;     // 16 bytes (size of first row of first matrix)
+    cout << "size of ***ptrx : " << sizeof(***ptrx) << endl;    // 4 bytes (size of first element)
+
+
+    cout << "size of ptry  : " << sizeof(ptry) << endl;         // pointer size (8 bytes)
+    cout << "size of *ptry : " << sizeof(*ptry) << endl;        // 96 bytes
+    cout << "size of **ptry : " << sizeof(**ptry) << endl;      // 48 bytes
+    cout << "size of ***ptry : " << sizeof(***ptry) << endl;    // 16 bytes
+    cout << "size of ****ptry : " << sizeof(****ptry) << endl;  // 4 bytes
+
+    for (int i=0; i<2; i++) {
+        for (int j=0; j<3; j++) {
+            for (int k=0; k<4; k++) {
+                cout << *( *( *(ptrx + i) + j ) + k ) << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+    for (int i=0; i<2; i++) {
+        for (int j=0; j<3; j++) {
+            for (int k=0; k<4; k++) {
+                cout << *( *( *(*ptry + i) + j ) + k ) << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+
+
+
+    cout << endl;
+    cout << "******** array of pointers ********" << endl;
+
+    // Simple analogy -
+    // int x;      // variable x of type int
+    // int x[3];   // variables x[0], x[1], x[2] of type int
+    // int *p;     // pointer p of type int *
+    // int *p[3];  // pointers p[0], p[1], p[2] of type int *
+    //             // In common words, Array of Pointers
+
+    // allocate 2D array using Array of Pointers
+    int *xptr[3];
+    int *yptr = xptr[0];
+    int **zptr = xptr;
+
+    // <dataType> * <pointer name> = new <dataType> [<size>] (init value);
+    // Dynamic 2D Array of Pointers -
+    // <dataType> **<Pointer name> = new <dataType> *[<size>]; 
+    int *i = new int;
+    int *j = new int();     // initialize with zero
+    int *k = new int[5];    // array of 5 integers
+    int *l = new int[5]();
+    int **m = new int *[5];
+
+    delete i;
+    delete j;
+    delete[] k;
+    delete[] l;
+    for (int i=0; i<5; i++) {
+        delete[] m[i];
+    }
+
+    int temp_count = 100;
+    int *pn = new int[3];
+
+    for (int i=0; i<5; i++) {
+        pn[i] = temp_count++;
+    }
+
+    for (int i=0; i<5; i++) {
+        cout << pn[i] << " ";
+    }
+
+    delete[] pn;
+    cout << endl;
+
+    // int m = 3, n = 4, c = 0;
+    
+
+}
