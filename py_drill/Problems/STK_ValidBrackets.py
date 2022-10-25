9f9211b63ffe9c6396b2edf149cfd8c69f58fe7a
PawanKmr470 <pawankmr470@gmail.com>
2022-10-26 02:46:29 +0530

    HashMap + Stack

---
+++ py_drill/Problems/STK_ValidBrackets.py
@@ -0,0 +1,32 @@
+# REF : https://leetcode.com/problems/valid-parentheses/
+# NOTES :
+#       Hashmap + stack
+
+# T: O(n)
+# S: O(n)
+
+from collections import deque
+
+class Solution:
+    def isValid(self, expn: str) -> bool:
+        s = deque()
+        hashmap = {'[':']', '{':'}', '(':')'}
+        rev_hashmap = {']': '[', '}': '{', ')': '('}
+        for i in range(len(expn)):
+            if expn[i] in hashmap.keys():
+                s.append(expn[i])
+                continue
+            if expn[i] in hashmap.values():
+                if s and rev_hashmap[expn[i]] == s[-1]:
+                    try:
+                        s.pop()
+                    except:
+                        return False
+                else:
+                    return False
+
+        return False if len(s) else True
+
+
+if __name__ == "__main__":
+    print(Solution().isValid("(]"))
