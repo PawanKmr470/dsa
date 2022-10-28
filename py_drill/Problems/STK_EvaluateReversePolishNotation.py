bf6fb4048c09abf9caed89d0e9c5579cde645521
PawanKmr470 <pawankmr470@gmail.com>
2022-10-29 04:21:26 +0530

    Simple Stack use

---
+++ py_drill/Problems/STK_EvaluateReversePolishNotation.py
@@ -0,0 +1,39 @@
+# REF : https://leetcode.com/problems/evaluate-reverse-polish-notation/
+# NOTES :
+#       Use stack. while traversing once the operator comes, pop two values,
+#       perform operation and push again into stack.
+
+# T: O()
+# S: O()
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for x in tokens:
+            if x not in ["+", "-", "*", "/"]:
+                stack.append(int(x))
+            elif x == "+":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(a + b)
+            elif x == "-":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(b - a)
+            elif x == "*":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(b * a)
+            elif x == "/":
+                a = stack.pop()
+                b = stack.pop()
+                stack.append(int(b / a))
+
+        return stack[0]
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
