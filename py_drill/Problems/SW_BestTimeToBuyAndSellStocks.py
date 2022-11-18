7a372c6c14bae03671c259e45d02d1d7ac1c05df
PawanKmr470 <pawankmr470@gmail.com>
2022-11-18 14:03:43 +0530

    Sliding Window, 2 pointers - left for min and right for max

---
+++ py_drill/Problems/SW_BestTimeToBuyAndSellStocks.py
@@ -0,0 +1,29 @@
+# REF : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# NOTES :
+#       Sliding window : if selling is low that is the next left ptr, if selling is high
+#       that is next right ptr
+
+# T: O(n)
+# S: O(1)
+
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        l, r = 0, 1
+        max_profit = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+            else:
+                l = r
+            r += 1
+
+        return max_profit
+
+
+def main():
+    prices = [7, 1, 5, 3, 6, 4]
+    print("output : {}".format(Solution().maxProfit(prices)))
+
+if __name__ == "__main__":
+    main()
